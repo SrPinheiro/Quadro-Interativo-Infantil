f481d5953f0bb3436d9d5e9409136b011ede5b24
srpinheiro <leonardogp.pinheiro@gmail.com>
2024-10-11 12:59:58 -0300

    RFID funcionando

--- include/rfidController/RFIDController.cpp
+++ include/rfidController/RFIDController.cpp
@@ -1,5 +1,40 @@
 #include "RFIDController.h"
+#include <Arduino.h>
 
-RFIDController::RFIDController(){
-    
+RFIDController::RFIDController() : mfrc522(portas[0], portas[4]) {
+    this->portas[0] = 10;  // SDA
+    this->portas[1] = 13;  // SCK
+    this->portas[2] = 11;  // MOSI
+    this->portas[3] = 12;  // MISO
+    this->portas[4] = 9;   // RST
+}
+
+void RFIDController::iniciar() {
+    SPI.begin();
+    mfrc522.PCD_Init();
+}
+
+String RFIDController::lerCodigoRFID() {
+    int contagem = 0; // essa contagem limita em 10 segundos para colocar o cartao no leitor :)
+
+    while(true){
+        if(contagem >= 20){
+            throw "[010] Limite de tempo para colocar o cartão"; // se passar 10 segundos e nao ler, ele lanca um erro
+        }
+
+        if (mfrc522.PICC_IsNewCardPresent()) {
+            break;
+        }
+        delay(500);
+    }
+
+    String codigo = ""; 
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+        codigo += (mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+        codigo += String(mfrc522.uid.uidByte[i], HEX);
+    }
+
+    mfrc522.PICC_HaltA();
+
+    return codigo;
 }
--- include/rfidController/RFIDController.h
+++ include/rfidController/RFIDController.h
@@ -1,9 +1,15 @@
 #ifndef RFIDController_H
 #define RFIDController_H
+#include <SPI.h>
+#include <MFRC522.h>
 
 class RFIDController {
     public:
+        int portas[5];
+        MFRC522 mfrc522;
         RFIDController();
+        void iniciar();
+        String lerCodigoRFID();
 };
 
 #endif
